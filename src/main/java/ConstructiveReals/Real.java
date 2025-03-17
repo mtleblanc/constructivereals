4a4da137931ef31aa6fa95bb777f5b4456a14f0f
Michael LeBlanc <leblanc.michael@gmail.com>
2025-03-17 13:18:48 -0400

    Add handling of floats/doubles and make subclass of Number

--- src/main/java/ConstructiveReals/Real.java
+++ src/main/java/ConstructiveReals/Real.java
@@ -2,7 +2,15 @@ package ConstructiveReals;
 
 import java.math.BigInteger;
 
-public interface Real {
+public abstract class Real extends Number {
+
+    private BigInteger estimate = BigInteger.ZERO;
+    private int estimatePrecision = Integer.MIN_VALUE;
+    private boolean estimateValid = false;
+    private int msd = 0;
+
+    
+
     /**
      * Returns a scaled approximation of the represented real.
      * | approximate(p) - x*2^p | < 1
@@ -10,71 +18,115 @@ public interface Real {
      * @param p precision as a power of 2. Usually negative
      * @return
      */
-    BigInteger approximate(int p);
-
-    public static Real of(long n) {
-        return new Constant(BigInteger.valueOf(n));
+    public synchronized BigInteger approximate(int p) {
+        if (estimateValid && p <= estimatePrecision) {
+            return estimate.shiftRight(estimatePrecision - p);
+        } else {
+            var newValue = computeApproximation(p);
+            estimatePrecision = p;
+            estimate = newValue;
+            estimateValid = true;
+            msd = newValue.bitLength() - p;
+            return newValue;
+        }
     }
 
-    public static Real of(BigInteger n) {
-        return new Constant(n);
+    public synchronized int size() {
+        return msd;
     }
 
-    default Real add(Real other) {
+    abstract BigInteger computeApproximation(int p);
+
+    public static Real valueOf(BigInteger n) {
+        return new BigIntegerConstant(n);
+    }
+
+    public static Real valueOf(long n) {
+        return valueOf(BigInteger.valueOf(n));
+    }
+
+    public static Real valueOf(double f) {
+        return new DoubleConstant(f);
+    }
+
+    public Real add(Real other) {
         return new Sum(this, other);
     }
 
-    default Real negate() {
+    public Real negate() {
         return new Negative(this);
     }
 
-    default Real subtract(Real other) {
+    public Real subtract(Real other) {
         return this.add(other.negate());
     }
 
-    default Real multiply(Real other) {
+    public Real multiply(Real other) {
         return new Product(this, other);
     }
 
-    default Real inverse() {
+    public Real inverse() {
         return new Inverse(this);
     }
 
-    default Real divide(Real other) {
+    public Real divide(Real other) {
         return new Product(this, other.inverse());
     }
 
-    public record Constant(BigInteger i) implements Real {
+    static class DoubleConstant extends Real {
+        private final BigInteger value;
+        private final int bias;
+
+        DoubleConstant(double d) {
+            long bits = Double.doubleToLongBits(d);
+            long mantissaBits = bits & ((1L << 52) - 1);
+            long exponentBits = bits & ((1L << 63) - 1) & ~((1L << 52) - 1);
+            if (exponentBits != 0) {
+                long scaledValue = mantissaBits | (1L << 52);
+                value = BigInteger.valueOf(bits < 0 ? -scaledValue : scaledValue);
+                bias = ((int) (exponentBits >> 52)) - 1023 - 52;
+            } else {
+                value = BigInteger.valueOf(bits < 0 ? -mantissaBits : mantissaBits);
+                bias = -1022 - 52;
+            }
+        }
+
+        @Override
+        public BigInteger approximate(int p) {
+            return value.shiftLeft(p + bias);
+        }
+
+        @Override
+        BigInteger computeApproximation(int p) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'computeApproximation'");
+        }
+    }
+
+    static class BigIntegerConstant extends Real {
+        private final BigInteger i;
+
+        BigIntegerConstant(BigInteger i) {
+            this.i = i;
+        }
+
         @Override
         public BigInteger approximate(int p) {
             return i.shiftLeft(p);
         }
-    }
-
-    abstract static class Cached implements Real {
-        BigInteger mostPrecise = BigInteger.ZERO;
-        int precision = Integer.MIN_VALUE;
-
-        abstract BigInteger computeApproximation(int p);
 
         @Override
-        public synchronized BigInteger approximate(int p) {
-            if (p <= precision) {
-                return mostPrecise.shiftRight(precision - p);
-            } else {
-                var newValue = computeApproximation(p);
-                precision = p;
-                mostPrecise = newValue;
-                return newValue;
-            }
+        BigInteger computeApproximation(int p) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'computeApproximation'");
         }
     }
 
-    public class Sum extends Cached {
+    static class Sum extends Real {
         private final Real left;
         private final Real right;
 
-        public Sum(Real left, Real right) {
+        Sum(Real left, Real right) {
             this.left = left;
             this.right = right;
         }
@@ -86,38 +138,50 @@ public interface Real {
 
     }
 
-    public record Negative(Real other) implements Real {
+    static class Negative extends Real {
+        private final Real other;
+
+        Negative(Real other) {
+            this.other = other;
+        }
+
         @Override
         public BigInteger approximate(int p) {
             return other.approximate(p).negate();
         }
+
+        @Override
+        BigInteger computeApproximation(int p) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'computeApproximation'");
+        }
     }
 
-    public class Product extends Cached {
+    static class Product extends Real {
         private final Real left;
         private final Real right;
 
-        public Product(Real left, Real right) {
+        Product(Real left, Real right) {
             this.left = left;
             this.right = right;
         }
 
         @Override
         BigInteger computeApproximation(int p) {
-            int leftPrecision = (p + 3) / 2;
-            int rightPrecision = p + 3 - leftPrecision;
+            int leftPrecision = (p + 4) / 2;
+            int rightPrecision = p + 4 - leftPrecision;
             BigInteger leftApproximation = left.approximate(leftPrecision);
             BigInteger rightApproximation = right.approximate(rightPrecision);
-            while (leftApproximation.bitLength() < 2) {
+            while (leftApproximation.bitLength() < 3) {
                 leftPrecision += 1;
                 leftApproximation = left.approximate(leftPrecision);
             }
-            while (rightApproximation.bitLength() < 2) {
+            while (rightApproximation.bitLength() < 3) {
                 rightPrecision += 1;
                 rightApproximation = left.approximate(rightPrecision);
             }
             int extraPrecision = leftPrecision + rightPrecision - p;
-            int leftMissing = leftApproximation.bitLength() + 3 - extraPrecision;
+            int leftMissing = leftApproximation.bitLength() + 4 - extraPrecision;
             if (leftMissing > 0) {
                 // This is correct. The right error is multiplied by the left value and vice
                 // versa
@@ -135,10 +199,10 @@ public interface Real {
         }
     }
 
-    public class Inverse extends Cached {
+    static class Inverse extends Real {
         private final Real other;
 
-        public Inverse(Real other) {
+        Inverse(Real other) {
             this.other = other;
         }
 
@@ -146,16 +210,69 @@ public interface Real {
         BigInteger computeApproximation(int p) {
             int q = 0;
             BigInteger a = other.approximate(q);
-            while (a.bitLength() < 1) {
+            while (a.bitLength() < 2) {
                 q += 1;
                 a = other.approximate(q);
             }
-            int deficit = a.bitLength() * 2 - p + q;
+            int deficit = -a.bitLength() * 2 + p + q + 2;
             if (deficit > 0) {
                 q += deficit;
                 a = other.approximate(q);
             }
-            return BigInteger.ONE.shiftLeft(p+q).divide(a);
+            return BigInteger.ONE.shiftLeft(p + q).divide(a);
+        }
+
+    }
+
+    @Override
+    public int intValue() {
+        return approximate(0).intValue();
+    }
+
+    @Override
+    public long longValue() {
+        return approximate(0).longValue();
+    }
+
+    @Override
+    public float floatValue() {
+        var p = 0;
+        var a = approximate(p);
+        while (a.bitLength() != 24) {
+            p += 24 - a.bitLength();
+            a = approximate(p);
+        }
+        int v = a.intValue();
+        boolean s = v < 0;
+        int sb = s ? 1 << 31 : 0;
+        int e = (-p + 127 + 23);
+        if (e > 0) {
+            int m = (s ? -v : v) - (1 << 23);
+            return Float.intBitsToFloat(sb | e << 23 | m);
+        } else {
+            int m = (s ? -v : v);
+            return Float.intBitsToFloat(sb | ((m >> -e) >> 1)); 
+        }
+    }
+
+    @Override
+    public double doubleValue() {
+        var p = 0;
+        var a = approximate(p);
+        while (a.bitLength() != 53) {
+            p += 53 - a.bitLength();
+            a = approximate(p);
+        }
+        long v = a.longValue();
+        boolean s = v < 0;
+        long sb = s ? 1L << 63 : 0;
+        long e = (-p + 1023L + 52L);
+        if (e > 0) {
+            long m = (s ? -v : v) - (1L << 52);
+            return Double.longBitsToDouble(sb | e << 52 | m);
+        } else {
+            long m = (s ? -v : v);
+            return Double.longBitsToDouble(sb | ((m >> -e) >> 1));
         }
 
     }
--- src/test/java/ConstructiveReals/RealTest.java
+++ src/test/java/ConstructiveReals/RealTest.java
@@ -1,16 +1,27 @@
 package ConstructiveReals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 public class RealTest {
 
+    boolean validFloat(float approximation, double value) {
+        double widened = approximation;
+        double error = widened - value;
+        widened += error / 2;
+        return ((float) widened) == approximation;
+
+    }
+
     @Test
     void testConstant() {
-        var constant = Real.of(42);
+        var constant = Real.valueOf(42);
 
-        assertEquals(42, constant.approximate(0).longValue());
+        assertEquals(42, constant.longValue());
 
         assertEquals(84, constant.approximate(1).longValue());
 
@@ -23,9 +34,9 @@ public class RealTest {
     @Test
     void testSum() {
 
-        var val = Real.of(42).add(Real.of(13));
+        var val = Real.valueOf(42).add(Real.valueOf(13));
 
-        assertEquals(55, val.approximate(0).longValue());
+        assertEquals(55, val.longValue());
 
         assertEquals(110, val.approximate(1).longValue());
 
@@ -36,24 +47,138 @@ public class RealTest {
 
     @Test
     void testProduct() {
-        var val = Real.of(42).multiply(Real.of(13));
-        assertEquals(13*42, val.approximate(0).longValue());
+        var val = Real.valueOf(42).multiply(Real.valueOf(13));
+        assertEquals(13 * 42, val.longValue());
     }
 
-    @Test 
+    @Test
     void testBigPrecision() {
-        var big = Real.of(1<<62).multiply(Real.of(1<<62));
-        var val = big.add(Real.of(1)).add(big.negate());
-        assertEquals(1, val.approximate(0).longValue());
+        var big = Real.valueOf(1 << 62).multiply(Real.valueOf(1 << 62));
+        var val = big.add(Real.valueOf(1)).add(big.negate());
+        assertEquals(1, val.longValue());
+    }
+
+    @Test
+    void testSubnormalNumbers() {
+        // Test exponent 0 and 1. Both have the same p=-126, and exponent 1 has an
+        // implicit leading 1 in the same bit
+        for (int i = 1; i < 0x0090_0000; i += 16) {
+            float f = Float.intBitsToFloat(i);
+            Real positive = Real.valueOf(f);
+            Real negative = Real.valueOf(-f);
+            assertEquals(i, positive.approximate(149).intValue());
+            assertEquals(f, positive.floatValue());
+            assertEquals(-i, negative.approximate(149).intValue());
+            assertEquals(-f, negative.floatValue());
+        }
+
+        for (long i = 1L; i < 0x0090_0000L; i += 16) {
+            double d = Double.longBitsToDouble(i << 29);
+            Real positive = Real.valueOf(d);
+            Real negative = Real.valueOf(-d);
+            assertEquals(i, positive.approximate(1022 + 52 - 29).longValue());
+            assertEquals(d, positive.doubleValue());
+            assertEquals(-i, negative.approximate(1022 + 52 - 29).longValue());
+            assertEquals(-d, negative.doubleValue());
+        }
     }
 
     @Test
     void testInverse() {
-        var val = Real.of(3).inverse();
-        assertEquals(1, val.approximate(2).longValue());
+        var val = Real.valueOf(3).inverse();
+        assertEquals(0.3333333f, val.floatValue());
+        assertEquals(0.3333333333333333, val.doubleValue());
 
-        var tough = Real.of(7321).divide(Real.of(992838488282L));
-        assertEquals(8107, tough.approximate(40).longValue());
+        var val2 = Real.valueOf(300000000L).inverse();
+        assertEquals(0.0000000033333332f, val2.floatValue());
+        var r = new Random();
+        for (int i = 0; i < 100; i++) {
+            double d = Double.longBitsToDouble(r.nextLong());
+            // double d = -31057005286.479670;
+            var original = Real.valueOf(d);
+            var doubleInverse = original.inverse().inverse();
+            assertEquals(d, doubleInverse.doubleValue());
+        }
+    }
 
+    @Test
+    void testValueOfDouble() {
+        var r = new Random();
+        for (int i = 0; i < 1000; i++) {
+            double d = Double.longBitsToDouble(r.nextLong());
+            if (!Double.isFinite(d)) {
+                continue;
+            }
+            var val = Real.valueOf(d);
+            assertEquals(d, val.doubleValue(), () -> "Found double not returning itself: " + Double.toHexString(d));
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            float f = Float.intBitsToFloat(r.nextInt());
+            if (!Float.isFinite(f)) {
+                continue;
+            }
+            var val = Real.valueOf(f);
+            assertEquals(f, val.floatValue(),
+                    () -> "Found float not returning itself: " + Integer.toHexString(Float.floatToIntBits(f)));
+        }
+    }
+
+    @Test
+    void testMultiplication() {
+        var r = new Random();
+        int successes = 0;
+        for (int i = 0; i < 1000; i++) {
+            double d1 = Float.intBitsToFloat(r.nextInt());
+            double d2 = Float.intBitsToFloat(r.nextInt());
+            double expected = d1 * d2;
+            float expectedFloat = (float) expected;
+            if (!Float.isFinite(expectedFloat) || expectedFloat == 0.0) {
+                continue;
+            }
+            var val = Real.valueOf(d1).multiply(Real.valueOf(d2));
+            float actual = val.floatValue();
+
+            long d1Bits = Double.doubleToLongBits(d1);
+            long d2Bits = Double.doubleToLongBits(d2);
+            long expectedBits = Double.doubleToLongBits(expected);
+            long actualBits = Double.doubleToLongBits(actual);
+            float error = (float) (((double) actual) - expected);
+            if(actual + error/2 != actual) {
+                var x = 7;
+            }
+            assertEquals(actual, actual + error / 2);
+            successes += 1;
+        }
+        assertTrue(successes > 20);
+    }
+
+    @Test
+    void testDivision() {
+        var r = new Random();
+        int successes = 0;
+        for (int i = 0; i < 100; i++) {
+            double d1 = Float.intBitsToFloat(r.nextInt());
+            double d2 = Float.intBitsToFloat(r.nextInt());
+            double expected = d1 / d2;
+            float expectedFloat = (float) expected;
+            if (!Float.isFinite(expectedFloat) || expectedFloat == 0.0) {
+                continue;
+            }
+            var val = Real.valueOf(d1).divide(Real.valueOf(d2));
+            float actual = val.floatValue();
+
+            long d1Bits = Double.doubleToLongBits(d1);
+            long d2Bits = Double.doubleToLongBits(d2);
+            long expectedBits = Double.doubleToLongBits(expected);
+            long actualBits = Double.doubleToLongBits(actual);
+            float error = (float) (((double) actual) - expected);
+            if(actual + error/2 != actual) {
+                var x = 7;
+            }
+            assertEquals(actual, actual + error / 2);
+            successes += 1;
+        }
+        assertTrue(successes > 20);
     }
 }
